c64a7cf988c60949f562c34736d477c86069e01d
Oxozon4 <oxozon.o4@gmail.com>
2020-05-15 16:02:02 +0200

    add current time display

--- 1.my-first-app/src/index.js
+++ 1.my-first-app/src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 const App = () => {
-    const buttonText = 'Click me';
+    const buttonText = { text: 'Click me!' };
+    function getTime() {
+        return (new Date()).toLocaleTimeString();
+    }
     return (
-        <div>
-            <label className="label" for="name">
+        <div style={{ textAlign: 'center' }}>
+            <div>
+                <div>Current Time:</div>
+                <h3>{getTime()}</h3>
+            </div>
+            <label className="label" htmlFor="name">
                 Enter name:
             </label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText.text}</button>
         </div>
     );
 };
