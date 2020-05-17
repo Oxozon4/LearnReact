64d54e9643a91f31c07ff88d5e2f6944099f66cd
oxozon-o4 <oxozon.o4@gmail.com>
2020-05-17 23:40:45 +0200

    pass time and content

--- 1.my-first-app/src/index.jsx
+++ 1.my-first-app/src/index.jsx
@@ -28,9 +28,9 @@ const App = () => {
         {buttonText.text}
       </button>
       <hr />
-      <CommentDetail author="Sam"/>
-      <CommentDetail author="Alex"/>
-      <CommentDetail author="Jane"/>
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post!"/>
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" content="I like the subject"/>
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="I like the writing"/>
     </div>
   );
 };
