b073a5560708e5bab18fccb3b872877068eee946
Oxozon4 <oxozon.o4@gmail.com>
2020-06-12 17:20:16 +0200

    add test label with text

--- 3.pictures/src/components/SearchBar.jsx
+++ 3.pictures/src/components/SearchBar.jsx
@@ -10,6 +10,7 @@ class SearchBar extends React.Component {
             <div className="ui segment">
                 <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
+                        <label>test</label>
                         <label>Image Search!</label>
                         <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })} />
                     </div>
